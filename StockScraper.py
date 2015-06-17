14492ee1df2e75c5226e0611bfcb20abecbc6ea5
Chris Liow <c.liow@claritypartners.com>
2015-06-17 10:16:38 -0500

    Multithreaded Implementation
    Requests to the website are now multithreaded, response time is
    significantly better

--- StockScraper.py
+++ StockScraper.py
@@ -1,25 +1,29 @@
 import urllib.request
 import re
-
+from GetCompanyData import getList
+from threading import Thread
 
 # Simple Yahoo Finance Web Scraper that retrieves current stock value for a company.
 # Regex currently only works for NASDAQ stocks
 # Linear implementation with 1 request made at a time, so cannot handle too many requests
 
-symbolList = ["aapl", "goog", "nflx", "amzn"]
-symbolList =  
+#symbolList = ["aapl", "goog", "nflx", "amzn"]
+symbolList = getList('csv')[1:]
+valueList = []
 valueDict = {}
-i = 0
-while(i < len(symbolList)):
-    regStockValue= '<span id=\"yfs_l84_' + symbolList[i] + '\">'
-    #print(regStockValue)
-    url = "http://finance.yahoo.com/q?s=" + symbolList[i] 
+activeThreads = []
+def th(symbol):
+    regStockValue= '<span id=\"yfs_l84_' + symbol + '\">'
+    url = "http://finance.yahoo.com/q?s=" + symbol
     htmlfile = urllib.request.urlopen(url)
-    htmltext = str(htmlfile.read())
-    price = re.split(regStockValue, htmltext)[1]
-    stockVal = price.split("</span>")[0]
-    #valueDict[symbolList[i]] = stockVal
+    htmltext = str(htmlfile.read()) 
+    price = re.split(regStockValue, htmltext)[1].split("</span>")[0]
+    print(price)
 
-    i += 1
+for s in symbolList:
+    request = Thread(target=th,args=(s,))
+    request.start()
+    activeThreads.append(request)
 
-print(valueDict)
+for b in activeThreads:
+    b.join()
